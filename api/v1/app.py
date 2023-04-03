cf056feffb7d9f41bc37951d62f4c12fc2ae62ba
linszay <lindseyjlancaster@gmail.com>
2023-04-03 17:21:48 -0500

    updated imports

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,6 +3,7 @@
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
+import os
 from os import getenv
 
 app = Flask(__name__)
@@ -16,6 +17,6 @@ def teardown_appcontext(exception):
 
 
 if __name__ == "__main__":
-    host = getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(getenv('HBNB_API_PORT', 5000))
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.getenv('HBNB_API_PORT', 5000))
     app.run(host=host, port=port, threaded=True)
