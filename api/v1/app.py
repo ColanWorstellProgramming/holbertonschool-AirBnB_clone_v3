fb7a4c4ee5a322166434bfa3f377bf40eaea7fc5
linszay <lindseyjlancaster@gmail.com>
2023-04-03 16:42:05 -0500

    added documenation to files index.py and app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-
+""" Creating an instance of Flask """
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
+from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix='/api/v1')
@@ -10,10 +11,11 @@ app.register_blueprint(app_views, url_prefix='/api/v1')
 
 @app.teardown_appcontext
 def teardown_appcontext(exception):
+    """method closes storage session"""
     storage.close()
 
 
 if __name__ == "__main__":
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.getenv('HBNB_API_PORT', 5000))
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(getenv('HBNB_API_PORT', 5000))
     app.run(host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-
+""" creates a JSON response """
 from flask import jsonify
 from api.v1.views import app_views
 
 
 @app_views.route('/status')
 def status():
+    """creates a JSON response for status message"""
     return jsonify({"status": "OK"})
