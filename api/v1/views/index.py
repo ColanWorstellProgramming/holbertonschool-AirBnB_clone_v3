2de60dfcc5201dbf7aefa2f3e20be26fd4513e98
linszay <lindseyjlancaster@gmail.com>
2023-04-03 17:34:02 -0500

    modified imports again

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """ creates a JSON response """
 from flask import jsonify
-from api.v1.views import app_views
 
 
 @app_views.route('/status')
 def status():
     """creates a JSON response for status message"""
+    from api.v1.views import app_views
     return jsonify({"status": "OK"})
