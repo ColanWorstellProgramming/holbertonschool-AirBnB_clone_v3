72de7b4545b79c6d0e4022e6749df4b8089963bf
Colan Worstell <Cworstellprogramming@gmail.com>
2023-04-03 17:05:56 -0700

    where

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Creating an instance of Flask """
-from flask import Flask
+from flask import Flask, make_response
 from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
@@ -12,18 +12,21 @@ CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views)
 
 
+@app.errorhandler(404)
+def handle_404(exception):
+    """handles 404 scenario (page not found)"""
+    code = exception.__str__().split()[0]
+    message = {"error": "Not found"}
+    return make_response(message, code)
+
+
 @app.teardown_appcontext
 def teardown_appcontext(exception):
     """method closes storage session"""
     storage.close()
 
 
-def start_flask():
-    """ start flask """
+if __name__ == "__main__":
     app.run(host=env('HBNB_API_HOST'),
             port=env('HBNB_API_PORT'),
             threaded=True)
-
-
-if __name__ == "__main__":
-    start_flask()
